f546c9f040fb46cec64d063a3c61e9f49c15ac20
ChloeLiang <nliangxin@gmail.com>
2018-10-16 09:13:33 +0800

    modify: app title

--- views/layout/Layout.jsx
+++ views/layout/Layout.jsx
@@ -12,7 +12,7 @@ class Layout extends React.Component {
           <link href="https://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet"></link>
           <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossOrigin="anonymous" />
           <link rel="stylesheet" type="text/css" href="/style/style.css"></link>
-          <title>Document</title>
+          <title>Goal Tracker</title>
         </head>
         <body>
           {this.props.children}
