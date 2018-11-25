fdf712387f6c21aa427e2e7144600042db98b614
ChloeLiang <nliangxin@gmail.com>
2018-11-25 20:26:59 +0800

    Add open graph meta tags

--- public/script/chart.js
+++ public/script/chart.js
@@ -1,12 +1,12 @@
-$(document).ready(function () {
+$(document).ready(function() {
   var labels = $('.detail').attr('data-labels');
   var data = $('.detail').attr('data-history');
-  console.log(data);
 
   var ctx = document.getElementById('myChart').getContext('2d');
   var chart = new Chart(ctx, {
     // The type of chart we want to create
     type: 'bar',
+
     // The data for our dataset
     data: {
       labels: labels.split(','),
@@ -14,35 +14,30 @@ $(document).ready(function () {
         {
           label: 'progress',
           backgroundColor: 'rgba(0, 0, 255, .7)',
-          data: data.split(',')
-        }
-      ]
+          data: data.split(','),
+        },
+      ],
     },
+
     // Configuration options go here
     options: {
       responsive: true,
       scales: {
-        xAxes: [{
-          maxBarThickness: 50
-        }],
-        yAxes: [{
-          display: true,
-          ticks: {
-            suggestedMin: 0,
-            beginAtZero: true
-          }
-        }]
-      }
-    }
+        xAxes: [
+          {
+            maxBarThickness: 50,
+          },
+        ],
+        yAxes: [
+          {
+            display: true,
+            ticks: {
+              suggestedMin: 0,
+              beginAtZero: true,
+            },
+          },
+        ],
+      },
+    },
   });
-  // var ajaxUrl = '/progress/' + $('.detail').attr('data-goalid');
-  // var request = new XMLHttpRequest();
-  // request.addEventListener('load', function () {
-  //   var progress = JSON.parse(this.responseText).progress;
-  //   var labels = progress
-
-  // });
-
-  // request.open('GET', ajaxUrl);
-  // request.send();
 });
--- views/layout/Layout.jsx
+++ views/layout/Layout.jsx
@@ -6,22 +6,67 @@ class Layout extends React.Component {
       <html lang="en">
         <head>
           <meta charSet="UTF-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
           <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
-          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossOrigin="anonymous"></link>
-          <link href="https://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet"></link>
-          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossOrigin="anonymous" />
-          <link rel="stylesheet" type="text/css" href="/style/style.css"></link>
-          <link rel="shortcut icon" type="image/png" href="https://png.icons8.com/cotton/50/000000/goal.png"></link>
+          <meta property="og:title" content="GoalTracker" />
+          <meta
+            property="og:description"
+            content="GoalTracker is designed to quantify your goal and break it into small tasks and schedule your every day task."
+          />
+          <meta
+            property="og:url"
+            content="https://goal-tracker-node.herokuapp.com/"
+          />
+          <meta
+            property="og:image"
+            content="https://res.cloudinary.com/nliangxin/image/upload/v1543044570/goal-tracker.png"
+          />
+          <link
+            rel="stylesheet"
+            href="https://use.fontawesome.com/releases/v5.3.1/css/all.css"
+            integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="https://fonts.googleapis.com/css?family=Lato:400,700"
+            rel="stylesheet"
+          />
+          <link
+            rel="stylesheet"
+            href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+            integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+            crossOrigin="anonymous"
+          />
+          <link rel="stylesheet" type="text/css" href="/style/style.css" />
+          <link
+            rel="shortcut icon"
+            type="image/png"
+            href="https://png.icons8.com/cotton/50/000000/goal.png"
+          />
           <title>Goal Tracker</title>
         </head>
         <body>
           {this.props.children}
-          <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossOrigin="anonymous"></script>
-          <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossOrigin="anonymous"></script>
-          <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossOrigin="anonymous"></script>
-          <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js"></script>
-          <script src="/script/script.js"></script>
+          <script
+            src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+            crossOrigin="anonymous"
+          />
+          <script
+            src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+            integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+            crossOrigin="anonymous"
+          />
+          <script
+            src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+            integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+            crossOrigin="anonymous"
+          />
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js" />
+          <script src="/script/script.js" />
         </body>
       </html>
     );
